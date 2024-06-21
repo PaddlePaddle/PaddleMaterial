24d8cdfcd26d31c32770c0bbfdeb738e308a4aa5
zhiminzhang0830 <452516515@qq.com>
2024-06-21 02:40:01 +0000

    update code

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ import argparse
 import yaml
 import paddle.distributed.fleet as fleet
 import paddle.distributed as dist
+from utils.misc import set_random_seed
 
 # To suppress warnings for clearer output
 warnings.simplefilter("ignore")
@@ -59,6 +60,101 @@ def load_dataset_from_pickle(structures_path, formation_energy_path):
     with open(formation_energy_path, 'rb') as f:
         formation_energy_per_atom = pickle.load(f)
     return structures, formation_energy_per_atom
+
+def get_dataloader(cfg):
+    # structures, mp_ids, eform_per_atom = load_dataset()
+    # structures = structures[:100]
+    # eform_per_atom = eform_per_atom[:100]
+
+    structures, eform_per_atom = load_dataset_from_pickle(
+        structures_path=cfg['dataset']['structures_path'],
+        formation_energy_path=cfg['dataset']['formation_energy_path'],
+    )
+
+    # get element types in the dataset
+    elem_list = get_element_list(structures)
+    # setup a graph converter
+    converter = Structure2Graph(element_types=elem_list, cutoff=cfg['dataset']['cutoff'])
+    # convert the raw dataset into MEGNetDataset
+    mp_dataset = MGLDataset(
+        structures=structures,
+        labels={"Eform": eform_per_atom},
+        converter=converter,
+        save_dir='./data/mp2018_cache'
+    )
+
+    train_data, val_data, test_data = split_dataset(
+        mp_dataset,
+        frac_list=cfg['dataset']['split_list'],
+        shuffle=True,
+        random_state=42,
+    )
+
+    train_loader = paddle.io.DataLoader(
+        train_data, 
+        batch_sampler=paddle.io.DistributedBatchSampler(
+            train_data,
+            batch_size=cfg['batch_size'],
+            shuffle=True,
+        ),
+        collate_fn=collate_fn_graph,
+        num_workers=0,
+    )
+    val_loader = paddle.io.DataLoader(
+        val_data, 
+        batch_sampler=paddle.io.DistributedBatchSampler(
+            val_data,
+            batch_size=cfg['batch_size'],
+        ),
+        collate_fn=collate_fn_graph,
+    )
+    test_loader = paddle.io.DataLoader(
+        test_data, 
+        batch_sampler=paddle.io.DistributedBatchSampler(
+            test_data,
+            batch_size=cfg['batch_size'],
+        ),
+        collate_fn=collate_fn_graph,
+    )
+
+    return train_loader, val_loader, test_loader
+
+
+def get_model(cfg):
+    # define the bond expansion
+    bond_expansion = BondExpansion(rbf_type="Gaussian", initial=0.0, final=5.0, num_centers=100, width=0.5)
+    # setup the architecture of MEGNet model
+    model = MEGNetPlus(
+        dim_node_embedding=cfg['model']['dim_node_embedding'],
+        dim_edge_embedding=cfg['model']['dim_edge_embedding'],
+        dim_state_embedding=cfg['model']['dim_state_embedding'],
+        nblocks=cfg['model']['nblocks'],
+        hidden_layer_sizes_input=cfg["model"]['hidden_layer_sizes_input'],
+        hidden_layer_sizes_conv=cfg["model"]['hidden_layer_sizes_conv'],
+        nlayers_set2set=cfg["model"]['nlayers_set2set'],
+        niters_set2set=cfg["model"]['niters_set2set'],
+        hidden_layer_sizes_output=cfg["model"]['hidden_layer_sizes_output'],
+        is_classification=cfg['model']['is_classification'],
+        activation_type=cfg['model']['activation_type'],
+        bond_expansion=bond_expansion,
+        cutoff=cfg['model']['cutoff'],
+        gauss_width=cfg['model']['gauss_width'],
+    )
+    model.set_dict(paddle.load('data/paddle_weight.pdparams'))
+
+    if dist.get_world_size() > 1:
+        model = fleet.distributed_model(model)
+    
+    return model
+
+def get_optimizer(cfg, model):
+
+    lr_scheduler = paddle.optimizer.lr.CosineAnnealingDecay(**cfg['lr_cfg'])
+    optimizer = paddle.optimizer.Adam(parameters=model.parameters(),
+        learning_rate=lr_scheduler, epsilon=1e-08, weight_decay=0.0)
+    if dist.get_world_size() > 1:
+        optimizer = fleet.distributed_optimizer(optimizer)
+    return optimizer, lr_scheduler
 
 def train_epoch(model, loader, loss_fn, optimizer, epoch, log):
     model.train()
@@ -101,101 +197,13 @@ def eval_epoch(model, loader, loss_fn, metric_fn, log):
     return sum(total_loss)/len(total_loss), sum(total_metric)/total_num_data
         
 
-def train(cfg, log):
-    fleet.init(is_collective=True)
-    # structures, mp_ids, eform_per_atom = load_dataset()
-    # structures = structures[:100]
-    # eform_per_atom = eform_per_atom[:100]
+def train(cfg):
+    log = init_logger(log_file=os.path.join(cfg['save_path'], 'train.log'))
+    train_loader, val_loader, test_loader = get_dataloader(cfg)
 
-    structures, eform_per_atom = load_dataset_from_pickle(
-        structures_path=cfg['dataset']['structures_path'],
-        formation_energy_path=cfg['dataset']['formation_energy_path'],
-    )
+    model = get_model(cfg)
+    optimizer, lr_scheduler = get_optimizer(cfg, model)
 
-    # get element types in the dataset
-    elem_list = get_element_list(structures)
-    # setup a graph converter
-    converter = Structure2Graph(element_types=elem_list, cutoff=cfg['dataset']['cutoff'])
-    # convert the raw dataset into MEGNetDataset
-    mp_dataset = MGLDataset(
-        structures=structures,
-        labels={"Eform": eform_per_atom},
-        converter=converter,
-        save_dir='./data/mp2018_cache'
-    )
-
-    train_data, val_data, test_data = split_dataset(
-        mp_dataset,
-        frac_list=cfg['dataset']['split_list'],
-        shuffle=True,
-        random_state=42,
-    )
-
-    # train_loader, val_loader, test_loader = MGLDataLoader(
-    #     train_data=train_data,
-    #     val_data=val_data,
-    #     test_data=test_data,
-    #     collate_fn=collate_fn_graph,
-    #     batch_size=cfg["batch_size"],
-    #     num_workers=0,
-    # )
-
-    train_loader = paddle.io.DataLoader(
-        train_data, 
-        batch_sampler=paddle.io.DistributedBatchSampler(
-            train_data,
-            batch_size=cfg['batch_size'],
-            shuffle=True,
-        ),
-        collate_fn=collate_fn_graph,
-        num_workers=0,
-    )
-    val_loader = paddle.io.DataLoader(
-        val_data, 
-        batch_sampler=paddle.io.DistributedBatchSampler(
-            val_data,
-            batch_size=cfg['batch_size'],
-        ),
-        collate_fn=collate_fn_graph,
-    )
-    test_loader = paddle.io.DataLoader(
-        test_data, 
-        batch_sampler=paddle.io.DistributedBatchSampler(
-            test_data,
-            batch_size=cfg['batch_size'],
-        ),
-        collate_fn=collate_fn_graph,
-    )
-
-    # define the bond expansion
-    bond_expansion = BondExpansion(rbf_type="Gaussian", initial=0.0, final=5.0, num_centers=100, width=0.5)
-    # setup the architecture of MEGNet model
-    model = MEGNetPlus(
-        dim_node_embedding=cfg['model']['dim_node_embedding'],
-        dim_edge_embedding=cfg['model']['dim_edge_embedding'],
-        dim_state_embedding=cfg['model']['dim_state_embedding'],
-        nblocks=cfg['model']['nblocks'],
-        hidden_layer_sizes_input=cfg["model"]['hidden_layer_sizes_input'],
-        hidden_layer_sizes_conv=cfg["model"]['hidden_layer_sizes_conv'],
-        nlayers_set2set=cfg["model"]['nlayers_set2set'],
-        niters_set2set=cfg["model"]['niters_set2set'],
-        hidden_layer_sizes_output=cfg["model"]['hidden_layer_sizes_output'],
-        is_classification=cfg['model']['is_classification'],
-        activation_type=cfg['model']['activation_type'],
-        bond_expansion=bond_expansion,
-        cutoff=cfg['model']['cutoff'],
-        gauss_width=cfg['model']['gauss_width'],
-    )
-    # model.set_dict(paddle.load('data/paddle_weight.pdparams'))
-
-    if dist.get_world_size() > 1:
-        model = fleet.distributed_model(model)
-
-    lr_scheduler = paddle.optimizer.lr.CosineAnnealingDecay(**cfg['lr_cfg'])
-    optimizer = paddle.optimizer.Adam(parameters=model.parameters(),
-        learning_rate=lr_scheduler, epsilon=1e-08, weight_decay=0.0)
-    if dist.get_world_size() > 1:
-        optimizer = fleet.distributed_optimizer(optimizer)
     loss_fn = paddle.nn.functional.mse_loss
     metric_fn = paddle.nn.functional.l1_loss
 
@@ -223,16 +231,55 @@ def train(cfg, log):
         log.info("test_loss: {:.6f}, test_mae: {:.6f}".format(test_loss.item(), test_metric.item()))
 
 
+def evaluate(cfg):
+    log = init_logger(log_file=os.path.join(cfg['save_path'], 'evaluate.log'))
+    train_loader, val_loader, test_loader = get_dataloader(cfg)
+
+    model = get_model(cfg)
+    optimizer, lr_scheduler = get_optimizer(cfg, model)
+
+    loss_fn = paddle.nn.functional.mse_loss
+    metric_fn = paddle.nn.functional.l1_loss
+
+    test_loss, test_metric = eval_epoch(model, val_loader, loss_fn, metric_fn, log)
+    log.info("eval_loss: {:.6f}, eval_mae: {:.6f}".format(test_loss.item(), test_metric.item()))
+
+
+def test(cfg):
+    log = init_logger(log_file=os.path.join(cfg['save_path'], 'test.log'))
+    train_loader, val_loader, test_loader = get_dataloader(cfg)
+
+    model = get_model(cfg)
+    optimizer, lr_scheduler = get_optimizer(cfg, model)
+
+    loss_fn = paddle.nn.functional.mse_loss
+    metric_fn = paddle.nn.functional.l1_loss
+
+    test_loss, test_metric = eval_epoch(model, test_loader, loss_fn, metric_fn, log)
+    log.info("test_loss: {:.6f}, test_mae: {:.6f}".format(test_loss.item(), test_metric.item()))
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config", type=str, default='./configs/megnet_3d.yaml', help="Path to config file")
+    parser.add_argument('--mode', type=str, default='train', choices=['train', 'eval', 'test'], required=True)
     args = parser.parse_args()
+
     with open(args.config, 'r') as f:
         cfg = yaml.safe_load(f)
+
     if paddle.distributed.get_rank() == 0:
         os.makedirs(cfg['save_path'], exist_ok=True)
         shutil.copy(args.config, cfg['save_path'])
-    log = init_logger(log_file=os.path.join(cfg['save_path'], 'run.log'))
-    train(cfg, log)
+    
+    set_random_seed(cfg.get('seed', 42))
+    
+    if args.mode == 'train':
+        train(cfg)
+    elif args.mode == 'eval':
+        evaluate(cfg)
+    elif args.mode == 'test':
+        test(cfg)
+    else:
+        raise ValueError("Unknown mode: {}".format(args.mode))
 
 
---
+++ utils/misc.py
@@ -0,0 +1,13 @@
+
+import paddle
+import random
+import numpy as np
+def set_random_seed(seed: int):
+    """Set numpy, random, paddle random_seed to given seed.
+
+    Args:
+        seed (int): Random seed.
+    """
+    paddle.seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
