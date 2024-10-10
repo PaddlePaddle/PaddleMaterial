8e6d5e44e7cd89fa31e216cdb8b277e12ce74075
zhiminzhang0830 <452516515@qq.com>
2024-10-10 15:16:44 +0800

    add guiduce for mattergen

--- structure_prediction/main.py
+++ structure_prediction/main.py
@@ -19,6 +19,7 @@ from models.diffusion import CSPDiffusionWithType
 from models.diffusion_with_guidance import CSPDiffusionWithGuidance
 from models.diffusion_with_guidance_d3pm import CSPDiffusionWithGuidanceD3PM
 from models.mattergen import MatterGen
+from models.mattergen import MatterGenWithGuidance
 from p_tqdm import p_map
 from pymatgen.core.lattice import Lattice
 from pymatgen.core.structure import Structure
@@ -92,6 +93,8 @@ def get_model(cfg):
         model = CSPDiffusionWithGuidanceD3PM(**model_cfg)
     elif model_name == "MatterGen":
         model = MatterGen(**model_cfg)
+    elif model_name == "MatterGenWithGuidance":
+        model = MatterGenWithGuidance(**model_cfg)
     else:
         model = CSPDiffusion(**model_cfg)
     # model.set_dict(paddle.load('data/paddle_weight.pdparams'))
