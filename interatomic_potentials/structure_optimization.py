8f16e8b72c050e86abc131ecf653abc56254b907
zhiminzhang0830 <452516515@qq.com>
2025-01-08 16:21:51 +0800

    feat: refine structure_optimization

--- interatomic_potentials/structure_optimization.py
+++ interatomic_potentials/structure_optimization.py
@@ -36,6 +36,18 @@ if __name__ == "__main__":
         default="./interatomic_potentials/configs/chgnet_2d_lessatom20_st.yaml",
         help="Path to config file",
     )
+    parser.add_argument(
+        "--cif_dir",
+        type=str,
+        default="./data/2d_1k/1000",
+        help="Path to cif directory",
+    )
+    parser.add_argument(
+        "--label_path",
+        type=str,
+        default=None,  # "./data/2d_1k/1000/relax.csv",
+        help="Path to label path",
+    )
 
     args, dynamic_args = parser.parse_known_args()
 
@@ -64,47 +76,83 @@ if __name__ == "__main__":
 
     relaxer = StructOptimizer(model=model)
 
-    cif_dir = "./data/2d_1k/1000"
+    cif_dir = args.cif_dir
     cif_files = [
         osp.join(cif_dir, f) for f in os.listdir(cif_dir) if f.endswith(".cif")
     ]
+    logger.info(f"Loaded {len(cif_files)} structures from {cif_dir}")
 
-    print(len(cif_files))
+    if args.label_path is not None:
+        label = pd.read_csv(args.label_path)
+        label_dict = {
+            key: value
+            for key, value in zip(label["cif"].tolist(), label["energy"].tolist())
+        }
+    else:
+        label_dict = None
 
-    label = pd.read_csv("./data/2d_1k/1000/relax.csv")
-    label_dict = {
-        key: value
-        for key, value in zip(label["cif"].tolist(), label["energy"].tolist())
+    cif_dir = os.path.join(config["Global"]["output_dir"], "relaxed_cifs")
+    os.makedirs(cif_dir, exist_ok=True)
+
+    preds = {
+        "cif_name": [],
+        "pred_energy_per_atom": [],
+        "true_energy_per_atom": [],
     }
-
-    preds = {}
     diff = []
-    index = 0
+
     for idx, cif_file in enumerate(cif_files):
 
-        base_name = os.path.basename(cif_file)
-        if base_name not in label_dict:
+        cif_name = os.path.basename(cif_file)
+
+        cif_name = os.path.basename(cif_file)
+        if label_dict is not None and cif_name not in label_dict:
             continue
-
         structure = Structure.from_file(cif_file)
-
         if max(structure.atomic_numbers) - 1 > 93:
             continue
         # Relax the structure
+        logger.info(f"Start relaxing structure: {cif_name}")
         result = relaxer.relax(structure, verbose=False)
 
-        preds[os.path.basename(cif_file)] = result["trajectory"].energies[-1]
+        relaxed_structure = result["final_structure"]
+        final_energy = result["trajectory"].energies[-1]
+        final_energy_per_atom = final_energy / len(structure)
+
+        optimized_cif_file_path = os.path.join(cif_dir, cif_name)
+        relaxed_structure.to(fmt="cif", filename=optimized_cif_file_path)
+        logger.info(f"Relaxed structure saved to {optimized_cif_file_path}")
+
+        preds["cif_name"].append(cif_name)
+        preds["pred_energy_per_atom"].append(final_energy_per_atom)
+
+        if label_dict is not None:
+            true_energy_per_atom = label_dict.get(cif_name)
+            if true_energy_per_atom is not None:
+                true_energy_per_atom = true_energy_per_atom / len(structure)
+        else:
+            true_energy_per_atom = None
+        preds["true_energy_per_atom"].append(true_energy_per_atom)
+
+        if true_energy_per_atom is not None:
+            diff.append(abs(final_energy_per_atom - true_energy_per_atom))
+
+        msg = (
+            f"idx: {idx}, cif name: {cif_name}, predicted energy per atom: "
+            f"{final_energy_per_atom}"
+        )
+        if true_energy_per_atom is not None:
+            msg += f", true energy per atom: {true_energy_per_atom}"
+            msg += f", diff: {abs(final_energy_per_atom - true_energy_per_atom)}"
+            msg += f", mae: {sum(diff)/len(diff)}"
+        logger.info(msg)
+
+        df = pd.DataFrame(preds)
+        df.to_csv(
+            osp.join(config["Global"]["output_dir"], "predictions.csv"), index=False
+        )
 
         logger.info(
-            f"preds and labels: {result['trajectory'].energies[-1]}, "
-            f"{label_dict[os.path.basename(cif_file)]}, n_elems: {structure.n_elems}, "
-            f"n_sites: {len(structure.sites)}"
+            "Prediction results saved to "
+            f"{osp.join(config['Global']['output_dir'], 'predictions.csv')}"
         )
-
-        diff.append(
-            abs(
-                result["trajectory"].energies[-1] / len(structure.sites)
-                - label_dict[os.path.basename(cif_file)] / len(structure.sites)
-            )
-        )
-        logger.info(f"idx: {idx}, mae: {sum(diff)/len(diff)}")
