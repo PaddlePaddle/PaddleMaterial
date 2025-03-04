2f89ba7d82ef5e34c99130becc5680d3e45fa083
zhiminzhang0830 <452516515@qq.com>
2025-03-04 03:39:44 +0000

    feat: modify evaluate

--- jointContribution/mattergen/scripts/evaluate.py
+++ jointContribution/mattergen/scripts/evaluate.py
@@ -12,30 +12,56 @@ from mattergen.evaluation.utils.structure_matcher import (
 
 def main(
     structures_path: str,
-    relax: bool = True,
+    relaxed_structures_path: (str | None) = None,
+    relax: bool = False,
     energies_path: (str | None) = None,
     structure_matcher: Literal["ordered", "disordered"] = "disordered",
     save_as: (str | None) = None,
-    potential_load_path: (
-        Literal["MatterSim-v1.0.0-1M.pth", "MatterSim-v1.0.0-5M.pth"] | None
-    ) = None,
-    device: str = "cuda",
 ):
     structures = load_structures(Path(structures_path))
+    if relaxed_structures_path is not None:
+        relaxed_structures = load_structures(Path(relaxed_structures_path))
+    else:
+        relaxed_structures = None
     energies = np.load(energies_path) if energies_path else None
     structure_matcher = (
         DefaultDisorderedStructureMatcher()
         if structure_matcher == "disordered"
         else DefaultOrderedStructureMatcher()
     )
+
+    reference_ele = {
+        'Sc', 'F', 'Pd', 'Ti', 'Nd', 'P', 'Ca', 'Ru', 'Sn', 'Sm', 'As', 'O', 'Be', 
+        'Au', 'Cd', 'Pt', 'Bi', 'Y', 'Si', 'Se', 'Cu', 'Sb', 'In', 'Br', 'Hf', 'I', 
+        'Ir', 'La', 'Ba', 'Er', 'Lu', 'W', 'Mo', 'Li', 'Ge', 'Pb', 'Hg', 'Tl', 'Ho', 
+        'Ta', 'Co', 'Ga', 'Nb', 'Fe', 'Mg', 'B', 'N', 'Cr', 'Sr', 'Rh', 'Yb', 'Ce', 
+        'Ni', 'Re', 'V', 'Os', 'H', 'Rb', 'Pr', 'Al', 'Eu', 'Cl', 'Gd', 'S', 'Ag', 
+        'Mn', 'Na', 'K', 'Zn', 'Cs', 'C', 'Te', 'Tb', 'Dy', 'Tm', 'Zr'}
+    new_structures = []
+    index = []
+    for i, structure in enumerate(structures):
+        flag = True
+        for site in structure:
+            if site.specie.symbol not in reference_ele:
+                flag = False
+                break
+        if flag:
+            new_structures.append(structure)
+            index.append(i)
+    print(f'{len(structures)} -> {len(new_structures)}')
+    n_failed_jobs = len(structures) - len(new_structures)
+    structures = new_structures
+    if energies is not None:
+        energies = [energies[i] for i in index]
+
     metrics = evaluate(
         structures=structures,
+        relaxed_structures=relaxed_structures,
         relax=relax,
         energies=energies,
         structure_matcher=structure_matcher,
         save_as=save_as,
-        potential_load_path=potential_load_path,
-        device=device,
+        n_failed_jobs=n_failed_jobs,
     )
     print(json.dumps(metrics, indent=2))
 
