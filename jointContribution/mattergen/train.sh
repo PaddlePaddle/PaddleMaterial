208a7583bbbf4179a4147a8390aac69800553bcf
zhiminzhang0830 <452516515@qq.com>
2025-03-25 11:16:13 +0000

    feat: add train script to train.sh

--- jointContribution/mattergen/train.sh
+++ jointContribution/mattergen/train.sh
@@ -15,3 +15,8 @@
 # --------------------------- 2d_30k----------------------------------#
 # PYTHONPATH=$PWD HYDRA_FULL_ERROR=1 python scripts/run.py data_module=2d_30k
 # PYTHONPATH=$PWD HYDRA_FULL_ERROR=1 python -m paddle.distributed.launch --gpus="4,5,6,7" scripts/run.py data_module=2d_30k
+
+
+# --------------------------- 2d_30k add mean distance----------------------------------#
+# PYTHONPATH=$PWD HYDRA_FULL_ERROR=1 python scripts/run.py --config-name=default_2d_md
+# PYTHONPATH=$PWD HYDRA_FULL_ERROR=1 python -m paddle.distributed.launch --gpus="4,5,6,7" scripts/run.py --config-name=default_2d_md
