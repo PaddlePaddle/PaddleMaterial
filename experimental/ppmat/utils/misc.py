93548dd4f52bde1d078e52dd7acd49ce8f719400
zhiminzhang0830 <452516515@qq.com>
2025-05-19 03:38:49 +0000

    feat: add ragged_range and repeat_blocks

--- experimental/ppmat/utils/misc.py
+++ experimental/ppmat/utils/misc.py
@@ -547,3 +547,142 @@ def run_at_rank0(func: Callable) -> Callable:
             return func(*args, **kwargs)
 
     return wrapped_func
+
+
+def ragged_range(sizes: paddle.Tensor) -> paddle.Tensor:
+    """Multiple concatenated ranges.
+
+    Examples
+    --------
+        sizes = [1 4 2 3]
+        Return: [0  0 1 2 3  0 1  0 1 2]
+    """
+    assert sizes.dim() == 1
+    if sizes.sum() == 0:
+        return paddle.empty(shape=[0], dtype=sizes.dtype)
+    sizes_nonzero = sizes > 0
+    if not paddle.all(x=sizes_nonzero):
+        sizes = paddle.masked_select(x=sizes, mask=sizes_nonzero)
+    id_steps = paddle.ones(shape=sizes.sum(), dtype="int64")
+    id_steps[0] = 0
+    insert_index = sizes[:-1].cumsum(axis=0)
+    insert_val = (1 - sizes)[:-1]
+    id_steps[insert_index] = insert_val
+    res = id_steps.cumsum(axis=0)
+    return res
+
+
+def repeat_blocks(
+    sizes: paddle.Tensor,
+    repeats: paddle.Tensor,
+    continuous_indexing: bool = True,
+    start_idx: int = 0,
+    block_inc: int = 0,
+    repeat_inc: int = 0,
+) -> paddle.Tensor:
+    """Repeat blocks of indices.
+    Adapted from https://stackoverflow.com/questions/51154989/numpy-vectorized-function-to-repeat-blocks-of-consecutive-elements
+
+    continuous_indexing: Whether to keep increasing the index after each block
+    start_idx: Starting index
+    block_inc: Number to increment by after each block,
+               either global or per block. Shape: len(sizes) - 1
+    repeat_inc: Number to increment by after each repetition,
+                either global or per block
+
+    Examples
+    --------
+        sizes = [1,3,2] ; repeats = [3,2,3] ; continuous_indexing = False
+        Return: [0 0 0  0 1 2 0 1 2  0 1 0 1 0 1]
+        sizes = [1,3,2] ; repeats = [3,2,3] ; continuous_indexing = True
+        Return: [0 0 0  1 2 3 1 2 3  4 5 4 5 4 5]
+        sizes = [1,3,2] ; repeats = [3,2,3] ; continuous_indexing = True ;
+        repeat_inc = 4
+        Return: [0 4 8  1 2 3 5 6 7  4 5 8 9 12 13]
+        sizes = [1,3,2] ; repeats = [3,2,3] ; continuous_indexing = True ;
+        start_idx = 5
+        Return: [5 5 5  6 7 8 6 7 8  9 10 9 10 9 10]
+        sizes = [1,3,2] ; repeats = [3,2,3] ; continuous_indexing = True ;
+        block_inc = 1
+        Return: [0 0 0  2 3 4 2 3 4  6 7 6 7 6 7]
+        sizes = [0,3,2] ; repeats = [3,2,3] ; continuous_indexing = True
+        Return: [0 1 2 0 1 2  3 4 3 4 3 4]
+        sizes = [2,3,2] ; repeats = [2,0,2] ; continuous_indexing = True
+        Return: [0 1 0 1  5 6 5 6]
+    """
+    assert sizes.dim() == 1
+    assert all(sizes >= 0)
+    sizes_nonzero = sizes > 0
+    if not paddle.all(x=sizes_nonzero):
+        assert block_inc == 0
+        sizes = paddle.masked_select(x=sizes, mask=sizes_nonzero)
+        if isinstance(repeats, paddle.Tensor):
+            repeats = paddle.masked_select(x=repeats, mask=sizes_nonzero)
+        if isinstance(repeat_inc, paddle.Tensor):
+            repeat_inc = paddle.masked_select(x=repeat_inc, mask=sizes_nonzero)
+    if isinstance(repeats, paddle.Tensor):
+        assert all(repeats >= 0)
+        insert_dummy = repeats[0] == 0
+        if insert_dummy:
+            one = paddle.ones(shape=[1], dtype=sizes.dtype)
+            zero = paddle.zeros(shape=[1], dtype=sizes.dtype)
+            sizes = paddle.concat(x=(one, sizes))
+            repeats = paddle.concat(x=(one, repeats))
+            if isinstance(block_inc, paddle.Tensor):
+                block_inc = paddle.concat(x=(zero, block_inc))
+            if isinstance(repeat_inc, paddle.Tensor):
+                repeat_inc = paddle.concat(x=(zero, repeat_inc))
+    else:
+        assert repeats >= 0
+        insert_dummy = False
+    r1 = paddle.repeat_interleave(x=paddle.arange(end=len(sizes)), repeats=repeats)
+    N = (sizes * repeats).sum()
+    id_ar = paddle.ones(shape=N, dtype="int64")
+    id_ar[0] = 0
+    insert_index = sizes[r1[:-1]].cumsum(axis=0)
+    insert_val = (1 - sizes)[r1[:-1]]
+    if isinstance(repeats, paddle.Tensor) and paddle.any(x=repeats == 0):
+        diffs = r1[1:] - r1[:-1]
+        indptr = paddle.concat(
+            x=(paddle.zeros(shape=[1], dtype=sizes.dtype), diffs.cumsum(axis=0))
+        )
+        if continuous_indexing:
+            # insert_val += segment_csr(sizes[: r1[-1]], indptr, reduce="sum")
+            raise NotImplementedError()
+        if isinstance(block_inc, paddle.Tensor):
+            # insert_val += segment_csr(block_inc[: r1[-1]], indptr, reduce="sum")
+            raise NotImplementedError()
+        else:
+            insert_val += block_inc * (indptr[1:] - indptr[:-1])
+            if insert_dummy:
+                insert_val[0] -= block_inc
+    else:
+        idx = r1[1:] != r1[:-1]
+        if continuous_indexing:
+            insert_val[idx] = 1
+        idx = paddle.where(condition=idx)[0].flatten()
+        insert_val[idx] += block_inc
+    if isinstance(repeat_inc, paddle.Tensor):
+        insert_val += repeat_inc[r1[:-1]]
+        if isinstance(repeats, paddle.Tensor):
+            repeat_inc_inner = repeat_inc[repeats > 0][:-1]
+        else:
+            repeat_inc_inner = repeat_inc[:-1]
+    else:
+        insert_val += repeat_inc
+        repeat_inc_inner = repeat_inc
+    if isinstance(repeats, paddle.Tensor):
+        repeats_inner = repeats[repeats > 0][:-1]
+    else:
+        repeats_inner = repeats
+    idx = r1[1:] != r1[:-1]
+    idx = paddle.where(condition=idx)[0].flatten()
+    insert_val[idx] -= repeat_inc_inner * repeats_inner
+    id_ar[insert_index] = insert_val
+    if insert_dummy:
+        id_ar = id_ar[1:]
+        if continuous_indexing:
+            id_ar[0] -= 1
+    id_ar[0] += start_idx
+    res = id_ar.cumsum(axis=0)
+    return res
