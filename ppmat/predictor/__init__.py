a34fef194c7748784f2408950488cc6a23be8ac6
JunnHuo <huojun1993@icloud.com>
2025-09-08 13:17:07 +0000

    Update training config (5th version)

--- ppmat/predictor/__init__.py
+++ ppmat/predictor/__init__.py
@@ -13,7 +13,9 @@
 # limitations under the License.
 
 from ppmat.predictor.base import BasePredictor
+from ppmat.predictor.sample import StructureSampler
 
 __all__ = [
     "BasePredictor",
+    "StructureSampler",
 ]
