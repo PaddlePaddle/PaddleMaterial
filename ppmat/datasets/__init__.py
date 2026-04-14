a07ffcafa1d3b3cf8bb75413a737d810f0b66344
r-cloudforge <266043551+r-cloudforge@users.noreply.github.com>
2026-04-14 13:25:50 +0200

    squash: preserve PR tree with signed identity

--- ppmat/datasets/__init__.py
+++ ppmat/datasets/__init__.py
@@ -47,6 +47,7 @@ from ppmat.datasets.num_atom_crystal_dataset import NumAtomsCrystalDataset
 from ppmat.datasets.oc20_s2ef_dataset import OC20S2EFDataset  # noqa
 from ppmat.datasets.qm9_dataset import QM9Dataset # noqa
 from ppmat.datasets.omol25_dataset import OMol25Dataset
+from ppmat.datasets.alloy_dataset import AlloyDataset
 from ppmat.datasets.split_mptrj_data import none_to_zero
 from ppmat.datasets.transform import build_transforms
 from ppmat.utils import logger
@@ -67,6 +68,7 @@ __all__ = [
     "DensityDataset", 
     "SmallDensityDataset",
     "OMol25Dataset",
+    "AlloyDataset",
 ]
 
 INFO_CLASS_REGISTRY: Dict[str, type] = {
