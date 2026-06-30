1ead2ce4cb21bdc4e06a5f6ee6333965e00fc353
PlumBlossomMaid <1589524335@qq.com>
2026-06-30 03:11:21 +0000

    chore: clean up md17 module docstring

--- ppmat/datasets/md17_dataset.py
+++ ppmat/datasets/md17_dataset.py
@@ -11,8 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""MD17 molecular dynamics dataset for energy and force prediction.
-"""
+"""MD17 molecular dynamics dataset for energy and force prediction."""
 
 import os
 import os.path as osp
